5e712e32a4c31307a7e5bbccac107c8c5a6c5833
purpleapplebear <purpleapplebear@yahoo.com>
2024-02-12 12:09:49 +0000

    updates property styles

--- src/components/PropertyList.jsx
+++ src/components/PropertyList.jsx
@@ -5,6 +5,7 @@ import { useState } from "react"
 import { fetchProperties } from "../api/api"
 import { Link } from "react-router-dom"
 import AddProperty from "./AddProperty"
+import './property-list-style.css'
 
 function PropertyList() {
     const [isLoading,setIsLoading]=useState(true)
@@ -19,26 +20,31 @@ function PropertyList() {
     },[setListOfProperties])
     
     return isLoading?<div>...is loading</div>:<div className="property-list-page">
-        <h2>list of properties</h2>
-        <div className="property-list-container">{listOfProperties.map(property=>{
-            return <ul key={property.address} className="property-list-item">
-               
-                <li><h3>address: {property.address}</h3></li>
-                <li><h4>{property.status}</h4></li>
-                <Link to={`/properties/${property.id}`}>
-                <li><img src={`${property.image}`} width='100vw' height="100vh" alt={`image of property at ${property.address}`}/></li>
-                </Link>
-                <li>Bedrooms ${property.bedroom}</li>
-                <li>price £{property.price}</li>
+        {listOfProperties.map(property=>{
+            return (
 
-            </ul>
+                <div class="property-wrapper">
+                <div class="ppt title"><Link to={`/properties/${property.id}`}><h1>{property.address}</h1></Link></div>
+                <div class="ppt image"><Link to={`/properties/${property.id}`}><img class="property-list-image" src={`${property.image}`} alt={`image of property at ${property.address}`}/></Link></div>
+                <div class="ppt description">{property.description}</div>
+                <div class="ppt content">
+                    <h2>£{property.price}</h2>
+                    <h3>{property.address}, {property.postcode}</h3>
+                    <p>Bedrooms: {property.bedroom}</p>
+                    <p>Bathrooms: {property.bathroom}</p>
+                    <p>{property.type}</p>
+                    <h4>{property.status}</h4>
+                </div>
+                </div>
+                )
+
         })}
         <div onClick={e=>{clicked?setClicked(false):null}}>{clicked?<div id="add-property-click">List Property Click here</div>:<AddProperty setListOfProperties={setListOfProperties} setClicked={setClicked}/>}</div>
         
         </div>
         
        
-    </div>
+
 }
 
 export default PropertyList
